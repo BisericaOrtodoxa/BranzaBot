f9318b6093fe6685604f65704a5338aadbc30420
ChonkMan <84081036+BisericaOrtodoxa@users.noreply.github.com>
2022-09-16 18:46:34 +0300

    first upload

---
+++ Branzabot.py
@@ -0,0 +1,34 @@
+import discord
+from googlesearch import search
+import os
+
+discord_token="MTAxOTY1NjYxNjkyMjA2MjkyOA.G38ax3.WXY-4wXnSNTJOoIKtBSYSGcT3otoolIn5x1_UE"
+#def cauta(quer):
+#for j in search(quer, num_results=1):
+#result=j
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if "porn" in message.content:
+        await message.channel.send("nu-i voie")     
+    if message.content.startswith('$branza'):
+        query = message.content
+        query_2 = query.replace("$branza", "") + " wikipedia.org"
+        for j in search(query_2, num_results=1):
+            await message.channel.send(j)
+
+my_secret = os.environ['discord_token']
+client.run(my_secret)
